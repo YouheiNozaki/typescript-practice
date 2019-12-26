07d4474e45d6fdb11e6d41202be4e19d326dd74d
Youhei Nozaki <youhey.hey0505@gmail.com>
2019-12-26 14:55:37 +0900

    CustomMapsをリファクタリング

--- practice1/src/CustomMap.ts
+++ practice1/src/CustomMap.ts
@@ -1,3 +1,13 @@
+// Instructions to every other class
+// on how they can be an argument to "addMarker"
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -6,8 +16,26 @@ export class CustomMap {
       zoom: 1,
       center: {
         lat: 0,
-        lng: 0
-      }
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
