82e11ad05a0a7b8b73cb077cee0b32d27e1fcf77
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-11 14:37:25 +0900

    bodyValidatorのdecoratorを作成.LoginControllerにValidatordecoratorを実装

--- server/src/controllers/LoginController.ts
+++ server/src/controllers/LoginController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { get, controller, use } from '../decorators';
+import { get, post, controller, use, bodyValidator } from '../decorators';
 
 function logger(req: Request, res: Response, next: NextFunction) {
   console.log('Request was made!!!');
@@ -23,5 +23,18 @@ class LoginController {
         <button>Submit</button>
       </form>
     `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email === 'hi@hi.com' && password === 'password') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+    } else {
+      res.send('Invalid email or password');
+    }
   }
 }
