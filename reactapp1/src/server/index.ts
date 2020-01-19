147ce97f70372229dc41d79a0b5e02ac36519662
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-19 21:54:48 +0900

    started server

---
+++ reactapp1/src/server/index.ts
@@ -0,0 +1,9 @@
+import Express from 'express';
+
+const app = Express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
