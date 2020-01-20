dfdb2204d9e963051bb1ab8bf7890d7fa9205fd8
Youhei Nozaki <youhey.hey0505@gmail.com>
2020-01-20 16:01:08 +0900

    create UserModel

---
+++ reactapp1/src/server/models/User.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+interface User {
+  name: String;
+  email: String;
+  password: String;
+  avatar: String;
+  date: Date;
+}
+
+const UserSchema = new mongoose.Schema<User>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('user', UserSchema);
+export default User;
